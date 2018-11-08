92a4928eefe93baa1b7a2a9c8675e7cd8c66effc
tianaraemamaril <tianaraetran@gmail.com>
2018-11-08 11:26:02 -0800

    Started on Linked Lists - added comments, some pseudocode, and some code.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,10 +3,45 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+  // if (initialValue) { // if the initial value exists
+  //   list.head = initialValue;
+  // }
+
+  // create a linkedListNode instance for head and tail
+
+
+
+  var node = new Node(value); // should Node Class be a different color?
+
   list.addToTail = function(value) {
+
+    // if list.head exists, assign value to head AND tail
+    // else assign value to tail 
+
+    if (list.head === null) {
+      list.head = node.value; 
+      list.tail = node.head;
+    } else {
+        
+        list.tail = value;
+    }
+    return list;
+    
   };
 
+
+// something with Node.prototype
   list.removeHead = function() {
+
+    // if (list.head) {
+    //   list.head = value;
+    //   delete list.head;
+    //   list.head = list.next;
+    // }
+    // return list;
+
+ 
+
   };
 
   list.contains = function(target) {
